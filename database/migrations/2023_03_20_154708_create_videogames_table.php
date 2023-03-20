208f370d006cbf93b0b86831a783566653ee4e87
PinoMacri <pinonegozio1@outlook.it>
2023-03-20 16:59:27 +0100

    nome colonne

---
+++ database/migrations/2023_03_20_154708_create_videogames_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videogames', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 30)->unique();
+            $table->float("price", 4 , 2);
+            $table->boolean("available");
+            $table->string("image")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videogames');
+    }
+};
