54d2366fb1edacddf02f12bc3435f554d698946e
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-03-20 17:46:08 +0100

    api for index

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Api\VideogamesApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/index', [VideogamesApiController::class, 'index']);
